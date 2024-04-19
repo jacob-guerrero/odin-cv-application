2aee5288ed81a107fac92d62b6ce5d8f3ee7466a
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-04-19 14:57:13 -0500

    Add form with general information section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import GeneralInfo from './GeneralInfo'
+
+function App() {
+
+  return (
+    <>
+    <form>
+       <GeneralInfo></GeneralInfo>
+    </form>
+    </>
+  )
+}
+
+export default App
---
+++ src/GeneralInfo.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+function GeneralInfo () {
+    const [nameValue, setNameValue] = useState("");
+    const [lastNameValue, setLastNameValue] = useState("");
+    const [emailValue, setEmailValue] = useState("");
+    const [phoneValue, setPhoneValue] = useState("");
+
+    const changeName = (e) => {
+        setNameValue(e.target.value) 
+    }
+    const changeLastName = (e) => {
+        setLastNameValue(e.target.value) 
+    }
+    const fullName = nameValue + ' ' + lastNameValue;
+
+    const changeEmail = (e) => {
+        setEmailValue(e.target.value) 
+    }
+    const changePhone = (e) => {
+        setPhoneValue(e.target.value) 
+    }
+
+    return (
+        <section className='general-info'>
+            <h2>General Information</h2>
+            
+            <label htmlFor="name">
+                Name:
+            </label>
+            <input
+                id='name'
+                type="text"
+                value={nameValue}
+                onChange={changeName}
+            />
+
+            <label htmlFor="lastName">
+                LastName:
+            </label>
+            <input
+                id='lastName'
+                type="text"
+                value={lastNameValue}
+                onChange={changeLastName}
+            />
+
+            <label htmlFor="email">
+                Email:
+            </label>
+            <input
+                id='email'
+                type="email"
+                value={emailValue}
+                onChange={changeEmail}
+            />
+
+            <label htmlFor="phone">
+                Phone:
+            </label>
+            <input
+                id='phone'
+                type="tel"
+                value={phoneValue}
+                onChange={changePhone}
+            />
+        </section>
+    )
+}
+
+export default GeneralInfo
